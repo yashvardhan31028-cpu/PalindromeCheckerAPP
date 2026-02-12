a3d0d47dc24243a45cee6267ddbe7df2e8c22262
yashvardhan31028-cpu <yashvardhan31028@gmail.com>
2026-02-12 10:25:19 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+    }
+}
